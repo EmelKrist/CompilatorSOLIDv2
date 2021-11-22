e5eabdeb95dc12f06f6f73d8330ef1648baff5bc
EmelKrist <kristina_1516@mail.ru>
2021-11-22 17:56:24 +0800

    Adding code

---
+++ CCompiler.java
@@ -0,0 +1,27 @@
+package compilatorsolidv2;
+
+public class CCompiler extends Compiler{
+    
+    //Реализуем все абстрактные методы класса Compiler
+    @Override
+    public void Lexer() {
+    System.out.println("\nЛексический анализ....");
+    }
+
+    @Override
+    public void Parser() {
+    System.out.println("Парсинг...."); 
+    }
+    
+    @Override
+    public void Optimizator() {
+    System.out.println("Оптимизация....");     
+    }
+
+    @Override
+    public void CodeGenerator() {
+    System.out.println("Генерация объектного кода....");   
+    
+    }
+       
+}
